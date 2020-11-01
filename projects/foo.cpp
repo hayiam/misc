51d15a026107e5da3d45fb2df391b2beafad927c
hayiam <72801928+hayiam@users.noreply.github.com>
2020-11-02 01:13:30 +0500

    Add files via upload

--- projects/foo.cpp
+++ projects/foo.cpp
@@ -21,19 +21,22 @@ class storyAtom
 // define story atoms objects
 storyAtom atom1;
 storyAtom atom2;
+storyAtom atom3;
 
 int main(int argc, char *argv[])
 {
     atom1.assignatm("first", "the first atom", "second");
     atom2.assignatm(atom1.nextitem, "the second atom", "third");
+    atom3.assignatm(atom2.nextitem, "the third atom", "fouth");
 
     // define a map
     std::map<std::string, storyAtom> storyAtoms;
 
     // fill the map with items
-    storyAtoms["first"] = atom1;
-    storyAtoms["second"] = atom2;
+    storyAtoms[atom1.atom] = atom1;
+    storyAtoms[atom1.nextitem] = atom2;
+    storyAtoms[atom2.nextitem] = atom3;
 
-    std::cout << "My first story atom is " << storyAtoms["second"].atom << std::endl;
+    std::cout << "My first story atom is " << storyAtoms["third"].atom << std::endl;
     return 0;
 }
