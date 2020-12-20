d05400438a36decb17606d3f9e20a20690feb40d
hayiam <72801928+hayiam@users.noreply.github.com>
2020-12-20 10:41:13 +0500

    Add files via upload

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
-compton --config ~/.config/compton/compton.conf &
+xset -dpms
+xset s off
+picom --experimental-backends --config .config/picom/picom.conf &
 ~/.fehbg &
 unclutter -root &
 
 while true; do
 
-    xsetroot -name " $(curl -s 'wttr.in/Orenburg?format=%t+%w\n') $(date +%a\ %F\ %R)"
+COMMAND=$(xset -q | grep LED | awk '{ print $10 }')
+case "$COMMAND" in
+ "00000000") LAYOUT="en" ;;
+ "00001000") LAYOUT="ru" ;;
+esac
+
+TIME=$(date +%a\ %F\ %R)
+
+    xsetroot -name " $LAYOUT $TIME "
     sleep 60
 
 done &
