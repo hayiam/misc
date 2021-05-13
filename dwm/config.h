0ba0364eff9198a3d2cb1d87d1533d57672ddfe0
hayiam <72801928+hayiam@users.noreply.github.com>
2021-05-14 00:00:51 +0500

    Add files via upload

--- dwm/config.h
+++ dwm/config.h
@@ -28,7 +28,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title                 tags mask     isfloating   monitor    scratchkey       float x,y,w,h */
-	{ NULL,       NULL,       "scratchpad",         0,            1,           -1,        's',             5,0,1593,400 },
+	{ NULL,       NULL,       "scratchpad",         0,            1,           -1,        's',             5,0,1585,400 },
 	{ NULL,       NULL,       "vifm_scratch",       0,            1,           -1,        'v',             150,200,1300,500 },
 };
 
