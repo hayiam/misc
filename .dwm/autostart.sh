56abb32c1b354cbe31b81673531016e08539063f
hayiam <72801928+hayiam@users.noreply.github.com>
2022-05-09 02:19:59 +0500

    Add files via upload

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -2,21 +2,22 @@
 
 xset -dpms
 xset s off
-picom --experimental-backends --config .config/picom/picom.conf &
+#compton --config .config/compton/compton.conf &
+picom --config .config/picom/picom.conf &
 ~/.fehbg &
 unclutter -root &
 
 # setting up dinamic wallpapers which will change every 60 seconds by using styli.sh script (sourse code of it: https://github.com/thevinter/styli.sh)
-while true; do
-
-sleep 1
-for i in "nature" "animals" "plants" "small town" "abstract"
-    do
-$HOME/misc/myde/styli.sh/styli.sh -b bg-scale -s "$i"
-sleep 1m
-done
-
-done &
+#while true; do
+#
+#feh --bg-fill pictures/wallpapers/wallpaper23.jpeg
+#for i in "nature" "animals" "plants" "small town" "abstract" "fantasy" "paintings"
+#    do
+#$HOME/misc/myde/styli.sh/styli.sh -b bg-scale -s "$i"
+#sleep 10m
+#done
+#
+#done &
 
 # setting up panel
 while true; do
