f7b08bc9b4be435024b0a41f054fd472b813051c
hayiam <72801928+hayiam@users.noreply.github.com>
2022-08-18 04:00:58 +0500

    Add files via upload

--- dwm/config.h
+++ dwm/config.h
@@ -41,6 +41,7 @@ static const Rule rules[] = {
 	/* class      instance    title                 tags mask     isfloating   monitor    scratchkey       float x,y,w,h */
 	{ NULL,       NULL,       "scratchpad",         0,            1,           -1,        's',             5,4,1585,400 },
 	{ NULL,       NULL,       "vifm_scratch",       0,            1,           -1,        'v',             150,200,1300,500 },
+	{ "xlunch",   NULL,       NULL,                 0,            1,           -1,        '\0',            5,4,1585,150 },
 };
 
 /* layout(s) */
@@ -85,12 +86,12 @@ static const char *ffox[] = { "ffox.sh", NULL };
 static const char *chrome[] = { "google-chrome-stable", "--start-fullscreen", NULL };
 /*static const char *chr[] = { "google-chrome-stable", NULL };*/
 static const char *scrot[] = { "scrot", "%Y-%m-%d-%H-%M-%S_$wx$h.png", "-e", "mv $f ~/pictures/screenshots", NULL };
-/*static const char *xlunch[] = { "xlunch", "-c", "~/.config/xlunch/xlunch.conf", "-i", ".config/xlunch/entries.dsv", NULL };*/
+static const char *xlunch[] = { "xlunch", "-c", "~/.config/xlunch/xlunch.conf", "-i", ".config/xlunch/entries.dsv", NULL };
 
 static Key keys[] = {
 	/* modifier                     key                function        argument */
 	{ MODKEY,                       XK_a,              spawn,          {.v = dmenucmd } },
-/*	{ MODKEY,                       XK_e,              spawn,          {.v = xlunch } },*/
+	{ MODKEY,                       XK_e,              spawn,          {.v = xlunch } },
 	{ MODKEY,                       XK_Return,         spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_grave,          togglescratch,  {.v = scratchpadcmd } },
 	{ 0,                            XK_F9,             togglescratch,  {.v = scratchpadvifm } },
@@ -179,7 +180,7 @@ static Key keys[] = {
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
-	{ ClkButton,	    	0,		        Button1,	    spawn,          {.v = dmenucmd } },
+	{ ClkButton,	    	0,		        Button1,	    spawn,          {.v = xlunch } },
 	{ ClkButton,	    	0,		        Button2,	    killclient,     {0} },
 	{ ClkButton,	    	0,		        Button3,	    reorganizetags, {0} },
     { ClkButton,	    	0,		        Button4,        setcfact,       {.f = +0.1} },
