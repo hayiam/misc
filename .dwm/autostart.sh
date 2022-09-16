5b996bd2c17d367289fb15d34da2d6df53170467
hayiam <72801928+hayiam@users.noreply.github.com>
2022-09-16 20:22:16 +0500

    Add files via upload

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -3,17 +3,18 @@
 xset -dpms
 xset s off
 #compton --config .config/compton/compton.conf &
-picom --experimental-backends --config .config/picom/picom.conf &
 ~/.fehbg &
+picom --experimental-backends --config .config/picom/picom.conf &
 unclutter -root &
 firefox-bin --headless &
+
 # setting up dinamic wallpapers which will change every 60 seconds by using styli.sh script (sourse code of it: https://github.com/thevinter/styli.sh)
 #while true; do
 #
-#feh --bg-fill pictures/wallpapers/wallpaper23.jpeg
+#feh --bg-fill pictures/wallpapers/wallpaper18.jpeg
 #for i in "nature" "animals" "plants" "small town" "abstract" "fantasy" "paintings"
 #    do
-#$HOME/misc/myde/styli.sh/styli.sh -b bg-scale -s "$i"
+#styli.sh -b bg-scale -s "$i"
 #sleep 10m
 #done
 #
@@ -26,6 +27,8 @@ COMMAND=$(xset -q | grep LED | awk '{ print $10 }')
 case "$COMMAND" in
  "00000000") LAYOUT="en" ;;
  "00001004") LAYOUT="ru" ;;
+ "00000002") LAYOUT="en" ;;
+ "00001006") LAYOUT="ru" ;;
 esac
 
 TIME=$(date +%a\ %F\ %R)
