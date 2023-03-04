894b613fd8079ce024cf89207e7d3f55cb0cc8c4
hayiam <72801928+hayiam@users.noreply.github.com>
2023-03-04 16:38:55 +0500

    Add files via upload

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,3 +2,5 @@ require("hm.base") -- General Settings
 require("hm.status") -- Statusline Setup
 require("hm.keymaps") -- Keymaps
 require("hm.funcs") -- Vim Function
+require("hm.plugins") -- Plugins
+require("hm.plugin_config") -- Plugin's configs
