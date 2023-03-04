1206398561a7bc909578958dd629366eb6be255f
hayiam <72801928+hayiam@users.noreply.github.com>
2023-03-04 07:07:51 +0500

    Add files via upload

---
+++ .config/nvim/lua/hm/funcs.lua
@@ -0,0 +1,60 @@
+--make tags and brackets autoclose and setup vim functions
+--autocmd FileType html inoremap <buffer> > ></<C-x><C-o><C-[>0f>a
+--autocmd FileType html inoremap <buffer> < <
+vim.cmd([[
+autocmd FileType c,cpp,h inoremap ( ()<C-[>%li
+autocmd FileType html inoremap < <><C-[>%li
+autocmd FileType c,cpp,h inoremap >>> ->
+autocmd FileType html,c,cpp,h inoremap { {<C-o>o}<C-[>%o
+autocmd FileType c,cpp,h inoremap [ []<C-[>%li
+
+function! FindTrailing()
+    execute '/\s\+$'
+endfunction
+
+function! FindTabs()
+    execute '/\t'
+endfunction
+
+autocmd cursorhold,bufwritepost * unlet! b:statusline_trailing_space_warning
+
+function! StatuslineTrailingSpaceWarning()
+    if !exists("b:statusline_trailing_space_warning")
+
+        if !&modifiable
+            let b:statusline_trailing_space_warning = ''
+            return b:statusline_trailing_space_warning
+        endif
+
+        if search('\s\+$', 'nw') != 0
+            let b:statusline_trailing_space_warning = '[tr] '
+        else
+            let b:statusline_trailing_space_warning = ''
+        endif
+    endif
+    return b:statusline_trailing_space_warning
+endfunction
+
+autocmd cursorhold,bufwritepost * unlet! b:statusline_tab_warning
+
+function! StatuslineTabWarning()
+    if !exists("b:statusline_tab_warning")
+        let b:statusline_tab_warning = ''
+
+        if !&modifiable
+            return b:statusline_tab_warning
+        endif
+
+        let tabs = search('^\t', 'nw') != 0
+
+        let spaces = search('^ \{' . &ts . ',}[^\t]', 'nw') != 0
+
+        if tabs && spaces
+            let b:statusline_tab_warning =  '[mi] '
+        else
+            let b:statusline_tab_warning = ' '
+        endif
+    endif
+    return b:statusline_tab_warning
+endfunction
+]])
---
+++ .config/nvim/lua/hm/keymaps.lua
@@ -0,0 +1,38 @@
+local function map(m, k, v)
+	vim.keymap.set(m, k, v, { silent = true })
+end
+--setup aliases for ctrl-c and ctrl-v
+map ("v", "<C-y>", '"+y')
+map ("n", "vv", '"+P')
+--remap ctrl+6 (to change keyboard layout) in insert mode
+map ("i", "<C-l>", "<C-^>")
+--ramap esc
+map ("i", "ii", "<C-[>")
+map ("v", "ii", "<C-[>")
+--remap * cgn for better find and replace word
+map ("n", "<C-]>", "*Ncgn")
+--omnicompletion shortcuts
+map ("i", "<C-o>", "<C-x><C-o>")
+--remap movement keys in insert mode
+map ("i", "<C-d>", "<Delete>")
+map ("i", "<C-h>", "<Left>")
+map ("i", "<C-l>", "<Right>")
+map ("i", "<C-k>", "<Up>")
+map ("i", "<C-j>", "<Down>")
+map ("i", "<C-a>", "<Home>")
+map ("i", "<C-e>", "<End>")
+map ("i", "<C-b>", "<C-o>b")
+map ("i", "<C-w>", "<C-o>w")
+--press enter to insert new line in normal mode
+map ("n", "<CR>", "o<Esc>")
+--setup hotkeys for split mode
+--switch between instances
+map ("n", "<space>", "<c-w>w")
+--resize instances
+map ("n", "<C-Right>", "<CMD>vertical resize +1<CR>")
+map ("n", "<C-Left>", "<CMD>vertical resize -1<CR>")
+map ("n", "<C-Down>", "<CMD>resize +1<CR>")
+map ("n", "<C-Up>", "<CMD>resize -1<CR>")
+--remap keys for functions
+map ("n", "<F4>", "<CMD>call FindTrailing()<CR>")
+map ("n", "<F3>", "<CMD>call FindTabs()<CR>")
