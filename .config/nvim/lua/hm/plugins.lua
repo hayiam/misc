0c26d2a76e67823f340e687fac35402bf3baa3af
hayiam <72801928+hayiam@users.noreply.github.com>
2023-03-12 05:12:07 +0500

    Add files via upload

--- .config/nvim/lua/hm/plugins.lua
+++ .config/nvim/lua/hm/plugins.lua
@@ -2,4 +2,6 @@ return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
     use 'nvim-treesitter/nvim-treesitter'
     use 'vifm/vifm.vim'
+    use 'lukas-reineke/indent-blankline.nvim'
+    use 'ggandor/lightspeed.nvim'
 end)
