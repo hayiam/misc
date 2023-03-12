3fa8aec1c7675a194c353218b951f202a48465ce
hayiam <72801928+hayiam@users.noreply.github.com>
2023-03-12 05:11:13 +0500

    Add files via upload

--- .config/nvim/lua/hm/plugin_config/indent-blankline.lua
+++ .config/nvim/lua/hm/plugin_config/indent-blankline.lua
@@ -1,7 +1,4 @@
+vim.g.indent_blankline_filetype_exclude = {"text"}
 require("indent_blankline").setup {
     show_current_context = false,
 }
-
-vim.cmd ([[
-    let g:indent_blankline_filetype_exclude = ['text']
-]])
