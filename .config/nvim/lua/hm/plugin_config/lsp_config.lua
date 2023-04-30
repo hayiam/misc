ce01753afad52aae79d419d03e90d2c38df6dbbf
hayiam <72801928+hayiam@users.noreply.github.com>
2023-04-30 06:10:50 +0500

    Add files via upload

--- .config/nvim/lua/hm/plugin_config/lsp_config.lua
+++ .config/nvim/lua/hm/plugin_config/lsp_config.lua
@@ -1,19 +1,37 @@
 require("mason").setup()
 
 require("mason-lspconfig").setup({
-ensure_installled = { "clangd", "quick_lint_js", "pylsp", "html" },
+ensure_installled = { "clangd", "quick_lint_js", "pylsp", "tsserver", "html" },
 automatic_installation = true,
 })
 
 require("lspconfig").clangd.setup{}
 require("lspconfig").quick_lint_js.setup{}
 require("lspconfig").pylsp.setup{}
-require("lspconfig").html.setup{}
+require("lspconfig").tsserver.setup{}
+require("lspconfig").html.setup{
+    on_attach = function ()
+                    -- reset omnicompletion function for html
+                    vim.cmd [[set omnifunc=htmlcomplete#CompleteTags]] 
+                end,
+    settings = { --requires enabling hints by force
+        css = {
+            lint = {
+                validProperties = {},
+            },
+        },
+        html = {
+            lint = {
+                validProperties = {},
+            },
+        },
+  },
+}
 
 -- start with lsp diagnostics disabled
 vim.api.nvim_create_autocmd(
-  "BufEnter",
-  { pattern = "*", command = "lua vim.diagnostic.disable()" }
+    "BufEnter",
+    { pattern = "*", command = "lua vim.diagnostic.disable()" }
 )
 -- configure diagnostics view
 vim.diagnostic.config({
