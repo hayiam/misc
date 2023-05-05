ccd8925acdc5fb4f9a1607e0f913a3fc0d176d53
hayiam <72801928+hayiam@users.noreply.github.com>
2023-05-05 10:53:49 +0500

    Add files via upload

---
+++ .config/nvim/lua/hm/plugin_config/telescope.lua
@@ -0,0 +1,33 @@
+function my_picker(results, title)
+  require("telescope.pickers").new({}, {
+  prompt_title = title,
+  finder = require("telescope.finders").new_table({
+  results = results,
+  }),
+  }):find()
+end
+
+require("telescope").setup {
+  extensions = {
+    menu = {
+      default = {
+        items = {
+          -- You can add an item of menu in the form of { "<display>", "<command>" }
+          { "Autoformat file", "lua vim.lsp.buf.format { async = true }" },
+          { "Change colorscheme", "Telescope colorscheme" },
+          { "Change all tabs to spaces", "call ReplaceTabs()" },
+          { "Remove trailing spaces", "call StripTrailing()" },
+          { "Add workspace folder", "lua vim.lsp.buf.add_workspace_folder()" },
+          { "Remove workspace folder", "lua vim.lsp.buf.remove_workspace_folder()" },
+          { "List workspace folders", "lua my_picker(vim.lsp.buf.list_workspace_folders(), 'Workspace folders')" },
+          { "Document symbols", "Telescope lsp_document_symbols" },
+          { "Workspace symbols", "Telescope lsp_workspace_symbols" },
+          { "Dynamic workspace symbols", "Telescope lsp_dynamic_workspace_symbols" },
+        },
+      },
+    },
+  },
+}
+
+require("telescope").load_extension("menu")
+require("telescope").load_extension("fzf")
