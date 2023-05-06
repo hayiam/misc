d8c76c5ff51b049662f0051b485f6d27b19ec62f
hayiam <72801928+hayiam@users.noreply.github.com>
2023-05-07 04:32:28 +0500

    Add files via upload

--- .config/nvim/lua/hm/plugin_config/lsp_config.lua
+++ .config/nvim/lua/hm/plugin_config/lsp_config.lua
@@ -41,3 +41,21 @@ vim.diagnostic.config({
         prefix = '✔', -- Could be '●', '▎', 'x', '■'
   }
 })
+-- configure floating lsp popups
+local _border = "single"
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover, {
+    border = _border
+  }
+)
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+  vim.lsp.handlers.signature_help, {
+    border = _border
+  }
+)
+
+vim.diagnostic.config{
+  float={border=_border}
+}
