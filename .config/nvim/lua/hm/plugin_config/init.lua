5627d2cd475d5b5c3d62920094ffa3bf33fc5d4c
hayiam <72801928+hayiam@users.noreply.github.com>
2023-05-07 04:36:58 +0500

    Delete init.lua

--- .config/nvim/lua/hm/plugin_config/init.lua
+++
@@ -1,6 +0,0 @@
-require("hm.plugin_config.treesitter")
-require("hm.plugin_config.vifm")
-require("hm.plugin_config.indent_blankline")
-require("hm.plugin_config.nvim_tree")
-require("hm.plugin_config.lsp_config")
-require("hm.plugin_config.telescope")
